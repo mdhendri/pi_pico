378c8c0d47232fd102e810f9b663dc33208c65d1
Michael Hendrickson <mdhendri@gmail.com>
2025-12-09 18:21:42 -0800

    Updating pwm example

--- Analog/analogin.py
+++ Analog/analogin.py
@@ -1,8 +1,12 @@
-from machine import ADC, Pin
+from machine import ADC, Pin, PWM
 import time
 
 # Initialize ADC (Analog to Digital Converter)
 adc = ADC(Pin(26))  # GP26 is ADC0 on the Raspberry Pi Pico
+value = 0
+#Using external LED, build PWM object PWM LED
+pwm_LED1=PWM(Pin(2))  # GPIO3 for PWM output to LED
+pwm_LED1.freq(500)
 
 while True:
     # Read the input on analog pin ADC0 (value between 0 and 65535)
@@ -23,4 +27,7 @@ while True:
 
     print(f"Analog reading: {value} - {description}")
 
-    time.sleep(0.5)  # delay for 500 milliseconds
+    new_value = 65535 - value  # Invert the value for LED brightness
+    print(new_value)
+    pwm_LED1.duty_u16(new_value)
+    time.sleep(0.1)  # delay for 500 milliseconds
